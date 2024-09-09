3419917455fee3e13e5ce123411486cb7769c14b
tomineuendorf <tomislavneuendorf@hotmail.de>
2024-09-09 11:57:05 +0200

    init Commit

---
+++ VoidVanguard/VoidVanguard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoidVanguard
+//
+//  Created by Tomi Neuendorf on 09.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoidVanguard/VoidVanguard/VoidVanguardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoidVanguardApp.swift
+//  VoidVanguard
+//
+//  Created by Tomi Neuendorf on 09.09.24.
+//
+
+import SwiftUI
+
+@main
+struct VoidVanguardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
