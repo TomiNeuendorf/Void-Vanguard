2d46a492ed1086d6efe8d9dbb17e62de1f1034b0
tomineuendorf <tomislavneuendorf@hotmail.de>
2024-09-12 16:38:03 +0200

    HomeScreen erstellt , Game Over Image Hinzugefügt , moving backround illusion created , Collision added between Enemy ship and player ship and player weapon and enemy ship  , player live added , score added

--- VoidVanguard/VoidVanguard/ContentView.swift
+++ VoidVanguard/VoidVanguard/ContentView.swift
@@ -1,42 +1,38 @@
-//
-//  ContentView.swift
-//  VoidVanguard
-//
-//  Created by Tomi Neuendorf on 09.09.24.
-//
-
 import SwiftUI
+import GameplayKit
 import SpriteKit
-import GameKit
+import Combine
+
+// New GameState class to manage the gameOver state
+class GameState: ObservableObject {
+    @Published var gameOver = false
+}
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    let background = SKSpriteNode(imageNamed:"Background-1")
+    // Use an external game state
+    var gameState: GameState? // Reference to GameState
+    
+    let background1 = SKSpriteNode(imageNamed: "Background-1")
+    let background2 = SKSpriteNode(imageNamed: "Background-1")
+    
     var player = SKSpriteNode()
-    var playerFire = SKSpriteNode()
     var fireTimer = Timer()
     var enemyTimer = Timer()
-    var enemy = SKSpriteNode()
     var score = 0
     var scoreLabel = SKLabelNode()
     var playerLive = [SKSpriteNode]()
     
-    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
-        scene?.size = CGSize(width: 750, height: 1335)
-        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        background.setScale(1.3)
-        background.zPosition = 1
-        addChild(background)
         
+        scene?.size = CGSize(width: 750, height: 1335)
+        
+        setupBackground()
         makePlayer(playerShip: 1)
         
         fireTimer = .scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playerFireFunction), userInfo: nil, repeats: true)
-        
         enemyTimer = .scheduledTimer(timeInterval: 1, target: self, selector: #selector(makeEnemys), userInfo: nil, repeats: true)
-        
-        //Score Text and Font
         
         scoreLabel.text = "Score: \(score)"
         scoreLabel.fontName = "Chalkduster"
@@ -47,106 +43,40 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(scoreLabel)
         
         addLives(lives: 3)
-        
     }
     
-    func didBegin(_ contact: SKPhysicsContact) {
-        let contactA: SKPhysicsBody
-        let contactB: SKPhysicsBody
+    func setupBackground() {
+        background1.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background1.setScale(1.3)
+        background1.zPosition = 0
+        addChild(background1)
         
-        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-            contactA = contact.bodyA
-            contactB = contact.bodyB
-        } else {
-            contactA = contact.bodyB
-            contactB = contact.bodyA
+        background2.position = CGPoint(x: size.width / 2, y: background1.position.y + background1.size.height)
+        background2.setScale(1.3)
+        background2.zPosition = 0
+        addChild(background2)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        moveBackground()
+    }
+    
+    func moveBackground() {
+        let backgroundSpeed: CGFloat = 3.0
+        background1.position.y -= backgroundSpeed
+        background2.position.y -= backgroundSpeed
+        
+        if background1.position.y < -background1.size.height / 2 {
+            background1.position.y = background2.position.y + background2.size.height
         }
-        
-        if contactA.categoryBitMask == CBitmask.playerFire && contactB.categoryBitMask == CBitmask.enemyShip {
-            playerFireHitEnemy(fires: contactA.node as! SKSpriteNode, enemys: contactB.node as! SKSpriteNode)
-            
-            updateScore()
+        if background2.position.y < -background2.size.height / 2 {
+            background2.position.y = background1.position.y + background1.size.height
         }
-        
-        if contactA.categoryBitMask == CBitmask.playerShip && contactB.categoryBitMask == CBitmask.enemyShip {
-            player.run(SKAction.repeat(SKAction.sequence([SKAction.fadeOut(withDuration: 0.1),SKAction.fadeIn(withDuration: 0.1)]),count: 8))
-            
-            contactB.node?.removeFromParent()
-            
-            if let live1 = childNode(withName: "live1") {
-                live1.removeFromParent()
-            } else if let live2 = childNode(withName: "live2") {
-                live2.removeFromParent()
-            } else if let live3 = childNode(withName: "live3") {
-                live3.removeFromParent()
-                player.removeFromParent()
-                fireTimer.invalidate()
-                enemyTimer.invalidate()
-            }
-        }
-    }
-    
-    func playerHitEnemy(players: SKSpriteNode, enemys: SKSpriteNode) {
-        players.removeFromParent()
-        enemys.removeFromParent()
-        
-        fireTimer.invalidate()
-        enemyTimer.invalidate()
-        
-        let explosion = SKEmitterNode(fileNamed: "Explosion")
-        explosion?.position = players.position
-        explosion?.zPosition = 5
-        addChild(explosion!)
-    }
-    
-    func playerFireHitEnemy(fires: SKSpriteNode, enemys: SKSpriteNode) {
-        fires.removeFromParent()
-        enemys.removeFromParent()
-        
-        let explosion = SKEmitterNode(fileNamed: "Explosion")
-        explosion?.position = enemys.position
-        explosion?.zPosition = 5
-        addChild(explosion!)
-    }
-    
-    func addLives(lives:Int) {
-        for i in 1...lives {
-            let live = SKSpriteNode(imageNamed: "heart")
-            live.setScale(2)
-            live.position = CGPoint(x: CGFloat(i) * live.size.width + 10, y: size.height - live.size.height - 10)
-            live.zPosition = 10
-            live.name = "live\(i)"
-            playerLive.append(live)
-            addChild(live)
-        }
-    }
-    
-    struct CBitmask {
-        static let playerShip: UInt32 = 0b1
-        static let playerFire: UInt32 = 0b10
-        static let enemyShip: UInt32 = 0b100
-        static let bossOne: UInt32 = 0b1000
     }
     
     func makePlayer(playerShip: Int) {
-        var shipname = ""
-        
-        switch playerShip {
-        case 1:
-            shipname = "ship_1"
-        case 2:
-            shipname = "ship_3"
-        case 3:
-            shipname = "ship_4"
-        case 4:
-            shipname = "ship_5"
-        case 5:
-            shipname = "ship_6"
-        default:
-            shipname = "ship_default"
-        }
-        
-        player = SKSpriteNode(imageNamed: shipname)  // Initialize player first
+        let shipname = "ship_\(playerShip)"
+        player = SKSpriteNode(imageNamed: shipname)
         player.position = CGPoint(x: size.width / 2, y: 120)
         player.zPosition = 10
         player.setScale(2)
@@ -160,7 +90,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     @objc func playerFireFunction() {
-        playerFire = SKSpriteNode(imageNamed: "shot")
+        let playerFire = SKSpriteNode(imageNamed: "shot")
         playerFire.position = player.position
         playerFire.zPosition = 2
         playerFire.physicsBody = SKPhysicsBody(rectangleOf: playerFire.size)
@@ -180,7 +110,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     @objc func makeEnemys() {
         let randomNumber = GKRandomDistribution(lowestValue: 50, highestValue: 700)
         
-        enemy = SKSpriteNode(imageNamed: "enemyShip_1")
+        let enemy = SKSpriteNode(imageNamed: "enemyShip_1")
         enemy.position = CGPoint(x: randomNumber.nextInt(), y: 1400)
         enemy.zPosition = 5
         enemy.setScale(0.2)
@@ -198,12 +128,72 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         enemy.run(combine)
     }
     
-    func updateScore () {
+    struct CBitmask {
+        static let playerShip: UInt32 = 0b1
+        static let playerFire: UInt32 = 0b10
+        static let enemyShip: UInt32 = 0b100
+        static let bossOne: UInt32 = 0b1000
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contactA: SKPhysicsBody
+        let contactB: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            contactA = contact.bodyA
+            contactB = contact.bodyB
+        } else {
+            contactA = contact.bodyB
+            contactB = contact.bodyA
+        }
+        
+        if contactA.categoryBitMask == CBitmask.playerFire && contactB.categoryBitMask == CBitmask.enemyShip {
+            playerFireHitEnemy(fires: contactA.node as! SKSpriteNode, enemys: contactB.node as! SKSpriteNode)
+            updateScore()
+        }
+        
+        if contactA.categoryBitMask == CBitmask.playerShip && contactB.categoryBitMask == CBitmask.enemyShip {
+            player.run(SKAction.repeat(SKAction.sequence([SKAction.fadeOut(withDuration: 0.1), SKAction.fadeIn(withDuration: 0.1)]), count: 8))
+            contactB.node?.removeFromParent()
+            if let live1 = childNode(withName: "live1") {
+                live1.removeFromParent()
+            } else if let live2 = childNode(withName: "live2") {
+                live2.removeFromParent()
+            } else if let live3 = childNode(withName: "live3") {
+                live3.removeFromParent()
+                player.removeFromParent()
+                fireTimer.invalidate()
+                enemyTimer.invalidate()
+                gameOverFunc()
+            }
+        }
+    }
+    
+    func playerFireHitEnemy(fires: SKSpriteNode, enemys: SKSpriteNode) {
+        fires.removeFromParent()
+        enemys.removeFromParent()
+        
+        let explosion = SKEmitterNode(fileNamed: "Explosion")
+        explosion?.position = enemys.position
+        explosion?.zPosition = 5
+        addChild(explosion!)
+    }
+    
+    func addLives(lives: Int) {
+        for i in 1...lives {
+            let live = SKSpriteNode(imageNamed: "heart")
+            live.setScale(2)
+            live.position = CGPoint(x: CGFloat(i) * live.size.width + 25, y: size.height - live.size.height - 10)
+            live.zPosition = 10
+            live.name = "live\(i)"
+            playerLive.append(live)
+            addChild(live)
+        }
+    }
+    
+    func updateScore() {
         score += 10
-        
         scoreLabel.text = "Score: \(score)"
-        
-        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -212,20 +202,68 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             player.position.x = location.x
         }
     }
+    
+    func gameOverFunc() {
+        removeAllChildren()
+        gameState?.gameOver = true // Notify SwiftUI that the game is over
+        
+    }
 }
 
 struct ContentView: View {
-    let scene = GameScene()
+    @ObservedObject var gameState = GameState() // Using the GameState
     
     var body: some View {
-        SpriteView(scene: scene)
-            .ignoresSafeArea()
-            .statusBar(hidden: true )
+        NavigationView {
+            ZStack {
+                // Game Scene (SpriteKit view)
+                SpriteView(scene: configureScene())
+                    .ignoresSafeArea()
+                
+                // Game Over overlay
+                if gameState.gameOver {
+                    ZStack{
+                        Color.myPurple
+                            .ignoresSafeArea()
+                        Image("GameOver2")
+                            .resizable()
+                            .scaledToFit()
+                            .ignoresSafeArea()
+                        VStack {
+                            Spacer()
+                            NavigationLink {
+                                HomeScreen()
+                                    .navigationBarHidden(true)
+                                    .navigationBarBackButtonHidden(true)
+                            } label: {
+                                Text("Back To Start")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Color.red)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 10)
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    // Helper function to configure the scene
+    func configureScene() -> SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 750, height: 1335)
+        scene.scaleMode = .aspectFill
+        scene.gameState = gameState // Link the scene to the game state
+        return scene
     }
 }
 
 #Preview {
     ContentView()
 }
-
 
--- VoidVanguard/VoidVanguard/HomeScreen.swift
+++ VoidVanguard/VoidVanguard/HomeScreen.swift
@@ -1,56 +1,101 @@
-//
-//  HomeScreen.swift
-//  VoidVanguard
-//
-//  Created by Tomi Neuendorf on 11.09.24.
-//
-
 import SwiftUI
 import SpriteKit
+
+struct SpaceBackground: UIViewRepresentable {
+    func makeUIView(context: Context) -> SKView {
+        let view = SKView()
+        let scene = SKScene(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        scene.scaleMode = .resizeFill
+        
+        // Static space background
+        let background = SKSpriteNode(imageNamed: "Void2")
+        background.position = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
+        background.zPosition = -1
+        
+        // Adjust the scale to fit the entire screen
+        let aspectRatio = background.size.width / background.size.height
+        background.size = CGSize(width: scene.size.height * aspectRatio, height: scene.size.height)
+        
+        scene.addChild(background)
+        
+        view.presentScene(scene)
+        return view
+    }
+    
+    func updateUIView(_ uiView: SKView, context: Context) {}
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color.purple, Color.black]),
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing)
+            )
+            .cornerRadius(15)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.black, lineWidth: 5)
+            )
+            .foregroundColor(.white)
+            .shadow(color: .purple, radius: configuration.isPressed ? 10 : 5)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
 
 struct HomeScreen: View {
     var body: some View {
         NavigationView {
             ZStack {
+                // Static space background
+                SpaceBackground()
+                    .ignoresSafeArea()
+                
                 VStack {
-                    Spacer()
                     
+                    // Title text with a glowing effect
                     Text("Void Vanguard")
-                        .font(.custom("Chalkduster", size: 45))
-                        .fontWeight(.bold)
+                        .font(.custom("Chalkduster", size: 40))
+                        .foregroundColor(.white)
+                        .shadow(color: .purple, radius: 5)
+                        .padding()
                     
                     Spacer()
-                    NavigationLink{
-                        ContentView().navigationBarHidden(true).navigationBarBackButtonHidden(true)
-                    } label: {
+                    
+                    // Start Game button with custom style
+                    NavigationLink(destination: ContentView().navigationBarHidden(true).navigationBarBackButtonHidden(true)) {
                         Text("Start Game")
-                            .foregroundColor(.red)
+                            .font(.headline)
+                            .fontWeight(.bold)
                     }
-                    Spacer()
+                    .buttonStyle(CustomButtonStyle())
+                    .padding(.bottom, 50)
                     
                     HStack {
-                        Button {
-                            
-                        } label: {
-                            Text("Ships")
+                        // Ships Button
+                        Button("Ships") {
+                            // Add action for Ships
                         }
+                        .buttonStyle(CustomButtonStyle())
                         .padding()
                         
-                        Button {
-                            
-                        } label: {
-                            Text("HighScores")
+                        // HighScores Button
+                        Button("H-Scores") {
+                            // Add action for HighScores
                         }
+                        .buttonStyle(CustomButtonStyle())
                         .padding()
                         
-                        Button {
-                            
-                        } label: {
-                            Text("Settings")
+                        // Settings Button
+                        Button("Settings") {
+                            // Add action for Settings
                         }
+                        .buttonStyle(CustomButtonStyle())
                         .padding()
-                        
                     }
+                    
                 }
             }
         }
@@ -60,3 +105,4 @@ struct HomeScreen: View {
 #Preview {
     HomeScreen()
 }
+
