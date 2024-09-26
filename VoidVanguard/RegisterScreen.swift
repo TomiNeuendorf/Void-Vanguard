e065f1e217c6e31ada94dff0d50ca5ff1bf5cffb
tomineuendorf <tomislavneuendorf@hotmail.de>
2024-09-26 16:21:41 +0200

    Added Register / Login Screen  and Onboarding Screen and did some minor changes

--- VoidVanguard/RegisterScreen.swift
+++ VoidVanguard/RegisterScreen.swift
@@ -1,5 +1,5 @@
 //
-//  LogInScreen.swift
+//  RegisterScreen.swift
 //  VoidVanguard
 //
 //  Created by Tomi Neuendorf on 26.09.24.
@@ -7,21 +7,22 @@
 
 import SwiftUI
 
-struct LoginScreen: View {
+struct RegisterScreen: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var confirmPassword: String = ""
     
     var body: some View {
         ZStack {
-            // Background: same space background as in HomeScreen and Onboarding
+            // Background: same space background as other screens
             SpaceBackground()
                 .ignoresSafeArea()
             
             VStack {
                 Spacer()
-
+                
                 // Title
-                Text("Login to Void Vanguard")
+                Text("Register for Void Vanguard")
                     .font(.custom("Chalkduster", size: 35))
                     .foregroundColor(.white)
                     .shadow(color: .purple, radius: 5)
@@ -31,7 +32,7 @@ struct LoginScreen: View {
                 VStack(alignment: .leading) {
                     Text("Email")
                         .foregroundColor(.white)
-                        .shadow(color: .black, radius: 10)
+                        .shadow(color: .black, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                         .font(.headline)
                         .padding(.leading)
                     
@@ -50,7 +51,7 @@ struct LoginScreen: View {
                 VStack(alignment: .leading) {
                     Text("Password")
                         .foregroundColor(.white)
-                        .shadow(color: .black, radius: 10)
+                        .shadow(color: .black, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                         .font(.headline)
                         .padding(.leading)
                     
@@ -61,13 +62,29 @@ struct LoginScreen: View {
                         .foregroundColor(.black)
                         .padding(.horizontal)
                 }
+                .padding(.bottom, 20)
+                
+                // Confirm Password Input Field
+                VStack(alignment: .leading) {
+                    Text("Confirm Password")
+                        .foregroundColor(.white)
+                        .shadow(color: .black, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .font(.headline)
+                        .padding(.leading)
+                    
+                    SecureField("Confirm your password", text: $confirmPassword)
+                        .padding()
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(10)
+                        .foregroundColor(.black)
+                        .padding(.horizontal)
+                }
                 .padding(.bottom, 40)
-
-                // Login Button
+                
+                // Register Button
                 Button(action: {
-                    // Handle login action
                 }) {
-                    Text("Login")
+                    Text("Register")
                         .font(.headline)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
@@ -81,13 +98,13 @@ struct LoginScreen: View {
                 
                 Spacer()
                 
-                // Sign Up option
+                // Login option for already registered users
                 HStack {
-                    Text("Don't have an account?")
+                    Text("Already have an account?")
                         .foregroundColor(.white)
                     
-                    NavigationLink(destination: RegisterScreen()) {
-                        Text("Sign Up")
+                    NavigationLink(destination: LoginScreen()) {
+                        Text("Login")
                             .foregroundColor(.purple)
                             .bold()
                     }
@@ -98,9 +115,8 @@ struct LoginScreen: View {
     }
 }
 
-struct LoginScreen_Previews: PreviewProvider {
+struct RegisterScreen_Previews: PreviewProvider {
     static var previews: some View {
-        LoginScreen()
+        RegisterScreen()
     }
 }
-
